8c179ea52fdf4d9c1854ce6ffe11af868b270842
HuyDao2007 <daong@whs.school.nz>
2024-05-20 14:54:20 +1200

    Add files via upload

--- BoatMoving.java
+++ BoatMoving.java
@@ -8,19 +8,14 @@ import java.util.Scanner;
 
 public class BoatMoving
 {
-    static void MainGrid()
+    static void MainGrid(int boatY, int boatX, int boatL, int i)
     {
        Scanner kb = new Scanner(System.in);
-       
-       boolean running = true;
        
        String[][] mainGrid = new String[10][10];
        String[] sideCoord = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "K"};
        
-       int boatX = 4;
-       int boatY = 0;
-       int x = 0;
-       while(running){
+       while(i == 0){
            System.out.print(" ");
            for(int a = 0; a < 10; a++)
            {
@@ -33,33 +28,41 @@ public class BoatMoving
                 for(int b = 0; b < 10; b++)
                 {
                 mainGrid[a][b] = ".";
-                if(b > x && b < boatX+1){
+                if(b < boatL && b > boatX-1){
                     mainGrid[boatY][b] = "B";
                 }
                 System.out.print(mainGrid[a][b] + " ");
                 }   
                 System.out.println();
            }  
-           String input = kb.nextLine();
-           if(input.equals("s")){
-               boatY += 1;
-           }
-           else if(input.equals("w")){
-               boatY -= 1;
-           }
-           else if(input.equals("d")){
-               boatX += 1;
-               x++;
-           }
-           else if(input.equals("a")){
-               boatX -= 1;
-               x--;
-           }
-           System.out.println('\u000c');
+           i++;
        }
     }
     public static void main(String[] args)
     {
-        MainGrid();
+        Scanner kb = new Scanner(System.in);
+        int boatY = 0; int boatX = 0; int boatL = 4;
+        boolean running = true;
+        MainGrid(boatY, boatX, boatL, 0);
+        while(running){
+            String input = kb.nextLine();
+            if(input.equals("w")){
+               boatY--;
+            }
+            else if(input.equals("s")){
+               boatY++;
+            }
+            else if(input.equals("d")){
+               boatX++; boatL++;
+            }
+            else if(input.equals("a")){
+                boatX--; boatL--;
+            }
+            else{
+                running = false;
+            }
+            System.out.println('\u000c');
+            MainGrid(boatY, boatX, boatL, 0);
+        }
     }
 }
