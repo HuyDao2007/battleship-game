8e4594835b043f4b85c891c446f2748ec12a728b
HuyDao2007 <daong@whs.school.nz>
2024-05-21 20:40:24 +1200

    Add files via upload

--- BoatMoving.java
+++ BoatMoving.java
@@ -8,45 +8,51 @@ import java.util.Scanner;
 
 public class BoatMoving
 {
-    static void MainGrid(int boatY, int boatX, int boatL, int i)
-    {
-       Scanner kb = new Scanner(System.in);
-       
+    static void MainGrid(int boatY, int boatX, int boatL, int r)
+    {       
        String[][] mainGrid = new String[10][10];
        String[] sideCoord = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "K"};
-       
-       while(i == 0){
-           System.out.print(" ");
+              
+       System.out.print(" ");
            for(int a = 0; a < 10; a++)
            {
             System.out.print(a+1 + " ");
-           }
-           System.out.println();
+       }
+       System.out.println();
            for(int a = 0; a < 10; a++)
-           {
+       {
                 System.out.print(sideCoord[a] + " ");
                 for(int b = 0; b < 10; b++)
                 {
                 mainGrid[a][b] = ".";
-                if(b < boatL && b > boatX-1){
+                if(b < boatL && b > boatX-1 && r == 0){
                     mainGrid[boatY][b] = "B";
+                }
+                if(a < boatL && r == 1){
+                    mainGrid[a][boatX] = "B";
                 }
                 System.out.print(mainGrid[a][b] + " ");
                 }   
                 System.out.println();
-           }  
-           i++;
-       }
+       }  
     }
     public static void main(String[] args)
     {
         Scanner kb = new Scanner(System.in);
-        int boatY = 0; int boatX = 0; int boatL = 4;
+        int boatY = 0; int boatX = 0; int boatL = 4; int r=0;
         boolean running = true;
-        MainGrid(boatY, boatX, boatL, 0);
+        MainGrid(boatY, boatX, boatL, r);
         while(running){
-            String input = kb.nextLine();
-            if(input.equals("w")){
+            String input = kb.nextLine(); 
+            if (input.equals("r"))
+            {
+                r++;
+            }
+            else if (input.equals("t"))
+            {
+                r--;
+            }
+            else if(input.equals("w")){
                boatY--;
             }
             else if(input.equals("s")){
@@ -62,7 +68,7 @@ public class BoatMoving
                 running = false;
             }
             System.out.println('\u000c');
-            MainGrid(boatY, boatX, boatL, 0);
+            MainGrid(boatY, boatX, boatL, r);
         }
     }
 }
--- MainGrid.java
+++ MainGrid.java
@@ -1,54 +1,74 @@
 /**
- * Write a description of class Battleship here.
+ * Battleship game
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Huy 
+ * @21/05/2024(a version number or a date)
  */
 import java.util.Scanner;
 
 public class MainGrid
 {
-    static void MainGrid()
-    {
-       Scanner kb = new Scanner(System.in);
-        
-        String[][] mainGrid = new String[10][10];
-        String[] sideCoord = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "K"};
-        
-        System.out.print(" ");
-        for(int a = 0; a < 10; a++)
-       {
+    static void MainGrid(int boatY, int boatX, int boatL, int r)
+    {       
+       String[][] mainGrid = new String[10][10];
+       String[] sideCoord = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "K"};
+              
+       System.out.print(" ");
+           for(int a = 0; a < 10; a++)
+           {
             System.out.print(a+1 + " ");
        }
-        System.out.println();
-        for(int a = 0; a < 10; a++)
-        {
-            System.out.print(sideCoord[a] + " ");
-            for(int b = 0; b < 10; b++)
-            {
+       System.out.println();
+           for(int a = 0; a < 10; a++)
+       {
+                System.out.print(sideCoord[a] + " ");
+                for(int b = 0; b < 10; b++)
+                {
                 mainGrid[a][b] = ".";
-                if(b < 4){
-                    mainGrid[0][b] = "B";
+                if(b < boatL && b > boatX-1 && r == 0){
+                    mainGrid[boatY][b] = "B";
                 }
-                if(b < 5){
-                    mainGrid[1][b] = "C";
-                }
-                if(b < 3){
-                    mainGrid[2][b] = "C";
-                }
-                if(b < 2){
-                    mainGrid[3][b] = "S";
-                }
-                if(b < 2){
-                    mainGrid[4][b] = "D";
+                if(a < boatL && r == 1){
+                    mainGrid[a][boatX] = "B";
                 }
                 System.out.print(mainGrid[a][b] + " ");
-            }
-            System.out.println();
-       } 
+                }   
+                System.out.println();
+       }  
     }
     public static void main(String[] args)
     {
-        
+        Scanner kb = new Scanner(System.in);
+        int boatY = 0; int boatX = 0; int boatL = 4; int r=0;
+        boolean running = true;
+        MainGrid(boatY, boatX, boatL, r);
+        while(running){
+            String input = kb.nextLine(); 
+            if (input.equals("r"))
+            {
+                r++;
+            }
+            else if (input.equals("t"))
+            {
+                r--;
+            }
+            else if(input.equals("w")){
+               boatY--;
+            }
+            else if(input.equals("s")){
+               boatY++;
+            }
+            else if(input.equals("d")){
+               boatX++; boatL++;
+            }
+            else if(input.equals("a")){
+                boatX--; boatL--;
+            }
+            else{
+                running = false;
+            }
+            System.out.println('\u000c');
+            MainGrid(boatY, boatX, boatL, r);
+        }
     }
 }
