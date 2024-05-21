27cbdd0f041645efbe6645a252f637852c1b644c
HuyDao2007 <daong@whs.school.nz>
2024-05-22 11:10:49 +1200

    Add files via upload

--- BoatMoving.java
+++ BoatMoving.java
@@ -36,37 +36,44 @@ public class BoatMoving
                 System.out.println();
        }  
     }
-    public static void Input()
+    public static void Input(int i)
     {
         Scanner kb = new Scanner(System.in);
-        int boatY = 0; int boatX = 0; int boatL = 4; int r=0;
-        boolean running = true;
-        MainGrid(boatY, boatX, boatL, r);
+        int boatY = 0; int boatX = 0; int boatL = 4; int r = 0;
+        boolean running = true; 
         while(running){
-            String input = kb.nextLine(); 
-
-            if (input.equals("r"))
+            if (i==1){
+                r=1;
+                String input = kb.nextLine(); 
+                if(input.equals("w")){
+                  boatY--; boatL--;
+                }
+                else if(input.equals("s")){
+                   boatY++; boatL++;
+                }
+                else if(input.equals("d")){
+                   boatX++; 
+                }
+                else if(input.equals("a")){
+                    boatX--; 
+                }
+            }
+            if(i==0)
             {
-                r++;
-            }
-            else if (input.equals("t"))
-            {
-                r--;
-            }
-            else if(input.equals("w")){
-               boatY--;
-            }
-            else if(input.equals("s")){
-               boatY++;
-            }
-            else if(input.equals("d")){
-               boatX++; 
-            }
-            else if(input.equals("a")){
-                boatX--; 
-            }
-            else{
-                running = false;
+                r=0;
+                String input = kb.nextLine(); 
+                if(input.equals("w")){
+                  boatY--;
+                }
+                else if(input.equals("s")){
+                   boatY++; 
+                }
+                else if(input.equals("d")){
+                   boatX++; boatL++;
+                }
+                else if(input.equals("a")){
+                    boatX--; boatL--;
+                }
             }
             System.out.println('\u000c');
             MainGrid(boatY, boatX, boatL, r);
@@ -74,7 +81,19 @@ public class BoatMoving
     }
     public static void main(String[] args)
     {
-        Input();
-    }   
-}
+        Scanner kb = new Scanner(System.in);
+        boolean running = true;
+        int boatY = 0; int boatX = 0; int boatL = 4; int r = 0; int i = 0;
+        MainGrid(boatY, boatX, boatL, r);
+        String input = kb.nextLine();
+        if(input.equals("r")){
+                i=0;
+        }
+        else if(input.equals("t")){
+                i=1;
+            }
+        Input(i);
+        }   
+    }
+
 
