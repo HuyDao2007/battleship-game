4886bb68784dd7626827b644aa86990733c7b7f3
HuyDao2007 <daong@whs.school.nz>
2024-05-28 12:00:39 +1200

    Add files via upload

--- BoatMoving.java
+++ BoatMoving.java
@@ -26,10 +26,10 @@ public class BoatMoving
                 {
                 mainGrid[a][b] = ".";
                 if(b < boatL1 && b > boatX1-1 && r == 0){
-                    mainGrid[boatY1][b] = "🚢?";
+                    mainGrid[boatY1][b] = "B";
                 }
                 if(a < boatL2 && a > boatY2-1 && r == 1){
-                    mainGrid[a][boatX2] = "🚢";
+                    mainGrid[a][boatX2] = "B";
                 }
                 System.out.print(mainGrid[a][b] + " ");
                 }   
@@ -50,8 +50,7 @@ public class BoatMoving
             }   
             else if(input.equals("t")) {
                 r=1;
-            }
-            
+            }    
             if(input.equals("w") && r==1){
                   boatY2--; boatL2--;
                 }
--- MainGame.java
+++ MainGame.java
@@ -8,6 +8,28 @@ import java.util.Scanner;
 
 public class MainGame
 {
+    static void Firing(int i, int j)
+    {       
+       String[][] mainGrid = new String[10][10];
+       String[] sideCoord = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "K"};
+       System.out.print(" ");
+       for(int a = 0; a < 10; a++)
+           {
+            System.out.print(a+1 + " ");
+       }
+       System.out.println();
+       for(int a = 0; a < 10; a++)
+           {
+            System.out.print(sideCoord[a] + " ");
+            for(int b = 0; b < 10; b++)
+                {
+               mainGrid[a][b] = ".";
+               mainGrid[i][j] = "X";
+               System.out.print(mainGrid[a][b] + " ");
+                }  
+            System.out.println();
+           }
+    }
     static void MainGrid(int boatY, int boatX, int boatL)
     {       
        String[][] mainGrid = new String[10][10];
@@ -73,31 +95,28 @@ public class MainGame
     public static void main(String[] args)
     {
         System.out.println('\u000c');
-        int i = 1;      
         Scanner kb = new Scanner(System.in);
+        
+        boolean shooting = true;
         
         System.out.println("Welcome to Battleship!");
         MainGrid(0, 0, 0);
         
         System.out.println("Please select your ship. Press number from 1 - 5 to spawn ships");
         System.out.println("Index: 1 = Destroyer, 2 = Submarine, 3 = Cruiser, 4 = Battleship, 5 = Carrier"); 
-        int ans = kb.nextInt();
-        switch(ans){
-            case 1:
-                Input(0, 0, 2);
-                break;
-            case 2:
-                Input(0, 0, 2);
-                break;
-            case 3:
-                Input(0, 0, 3);
-                break;
-            case 4:
-                Input(0, 0, 4);
-                break;
-            case 5:
-                Input(0, 0, 5);
-                break;
+        
+        
+        
+        while(shooting){
+            String[] coord = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "k"};
+            int a = 1; 
+            String y1 = kb.nextLine();
+            int y = kb.nextInt()-1;
+            while(!(y1.equals(coord[a]))){
+                a++;
+            }
+            int x = a;
+            Firing(x, y);
         }
     }
 }
